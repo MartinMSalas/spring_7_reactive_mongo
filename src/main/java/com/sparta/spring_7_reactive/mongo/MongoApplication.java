4127c621c4761458fc7f15fb193bf8d5f8d28696
MartinMSalas <ingmartinmsalas@gmail.com>
2026-04-01 19:28:10 -0300

    adding files

---
+++ src/main/java/com/sparta/spring_7_reactive/mongo/MongoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_7_reactive.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoApplication.class, args);
+	}
+
+}
